b4d1b45296f7dc2c3b50d80fe407469c72045e3a
Anirban Das <72315775+anirban12d@users.noreply.github.com>
2023-07-21 14:14:19 +0530

    modified src/root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -2,13 +2,17 @@ import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/head/head';
 import { inject } from '@vercel/analytics';
+import { isDev } from '@builder.io/qwik/build';
 
 
 import './global.css';
 
+
 export default component$(() => {
 
-  inject()
+  inject({
+    mode: isDev? 'development' : 'production',
+  })
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
