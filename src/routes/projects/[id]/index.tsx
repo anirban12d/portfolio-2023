dfc1668070789b689c31f03d9abcbd4d3b55fb67
Anirban Das <72315775+anirban12d@users.noreply.github.com>
2023-07-24 15:14:46 +0530

    Added Dynamic head for projects

--- src/routes/projects/[id]/index.tsx
+++ src/routes/projects/[id]/index.tsx
@@ -5,6 +5,7 @@ import { client } from "~/routes/layout";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import imageUrlBuilder from "@sanity/image-url";
 import { Image } from "@unpic/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export const useProjectData = routeLoader$(async ({ params }) => {
   // This code runs only on the server, after every navigation
@@ -331,16 +332,16 @@ export default component$(() => {
                 </h3>
 
                 <div class="flex flex-col items-center gap-16 lg:flex-row">
-                  <div class="flex self-stretch flex-col gap-8 rounded-4xl border-4 border-black bg-grey py-16 pl-32 pr-64 md:px-64 lg:py-32">
+                  <div class="flex flex-col gap-8 self-stretch rounded-4xl border-4 border-black bg-grey py-16 pl-32 pr-64 md:px-64 lg:py-32">
                     <h6 class="text-23 font-medium text-textColor md:text-27 lg:text-36">
                       Frontend Tools
                     </h6>
-                    <p class="text-16 font-medium text-textColor lg:max-w-384 max-w-prose">
+                    <p class="max-w-prose text-16 font-medium text-textColor lg:max-w-384">
                       {projectData.frontendTools}
                     </p>
                   </div>
-                  <div class="flex self-stretch flex-col gap-8 rounded-4xl border-4 border-black bg-grey py-16 pl-32 pr-64 md:px-64 lg:py-32">
-                    <h6 class="text-23 font-medium text-textColor md:text-27 lg:text-36 max-w-prose">
+                  <div class="flex flex-col gap-8 self-stretch rounded-4xl border-4 border-black bg-grey py-16 pl-32 pr-64 md:px-64 lg:py-32">
+                    <h6 class="max-w-prose text-23 font-medium text-textColor md:text-27 lg:text-36">
                       Ui Libraries
                     </h6>
                     <p class="text-16 font-medium text-textColor lg:max-w-384">
@@ -356,3 +357,85 @@ export default component$(() => {
     </>
   );
 });
+
+export const head: DocumentHead = ({ resolveValue, params }) => {
+  const data = resolveValue(useProjectData);
+  const projectData = data[0];
+  return {
+    title: `Project - ${projectData.title}`,
+    meta: [
+      {
+        name: "description",
+        content: `${projectData.description}`,
+      },
+      // Open graph
+      {
+        property: "og:title",
+        content: `Anirban Das - Project - ${projectData.title}`,
+      },
+      {
+        property: "og:description",
+        content: `${projectData.description}`,
+      },
+      {
+        property: "og:url",
+        content: `https://www.anirbandas.in/projects/${params.id}`, // Replace with your website URL
+      },
+      {
+        property: "og:locale",
+        content: "en",
+      },
+      {
+        property: "og:site_name",
+        content: "Anirban Das - Fullstack Developer & UX Designer",
+      },
+      {
+        property: "og:type",
+        content: "website",
+      },
+      {
+        property: "og:image",
+        content: `${urlFor(projectData.mainImage.asset).url()}`, // Replace with the actual image URL for sharing
+      },
+      {
+        property: "og:image:width",
+        content: "1200",
+      },
+      {
+        property: "og:image:height",
+        content: "630",
+      },
+      // Twitter Card meta tags (for Twitter sharing)
+      {
+        name: "twitter:card",
+        content: "summary_large_image",
+      },
+      {
+        name: "twitter:site",
+        content: "@anirban12d",
+      },
+      {
+        name: "twitter:title",
+        content: `Anirban Das - Projects - ${projectData.title}`,
+      },
+      {
+        name: "twitter:description",
+        content: `${projectData.description}`,
+      },
+      {
+        name: "twitter:image",
+        content: `${urlFor(projectData.mainImage.asset).url()}`, // Replace with the actual image URL for sharing
+      },
+      {
+        name: "twitter:image:alt",
+        content: `Image of Anirban Das's project ${projectData.title}'s homepage`,
+      },
+    ],
+    links: [
+      {
+        rel: "canonical",
+        href: `https://www.anirbandas.in/projects/${params.id}/`,
+      },
+    ],
+  };
+};
