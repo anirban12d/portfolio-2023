7fd58ffdf5dd5e4751f89349f6e4e7d5bbfb355b
Anirban Das <72315775+anirban12d@users.noreply.github.com>
2024-06-17 12:40:57 +0530

    vercel edge function fix

--- src/root.tsx
+++ src/root.tsx
@@ -71,7 +71,7 @@ export default component$(() => {
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css"
         />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-        {/* <script>hljs.highlightAll();</script>  */}
+        <script>hljs.highlightAll();</script>
       </head>
 
       <body lang="en">
